b95e7c375c8c24f76fdb727457f8f128b92b7b99
s-ly <SergeyLysov@outlook.com>
2022-01-12 19:15:02 +0300

    test1

---
+++ app1.py
@@ -0,0 +1,7 @@
+import time
+
+i = 0
+while True:
+    print('work app1 ' + str(i))
+    i = i + 1
+    time.sleep(1)
---
+++ app2.py
@@ -0,0 +1,7 @@
+import time
+
+i = 0
+while True:
+    print('work app2 ' + str(i))
+    i = i + 1
+    time.sleep(1)
