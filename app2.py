e1ca7973c7ad0478b047b4817458a0d77e1ab638
s-ly <SergeyLysov@outlook.com>
2022-03-15 18:04:09 +0300

    add port 2

--- app2.py
+++ app2.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/') 
 def test() -> str:
-    return 'hi'
+    return 'hi port 5002'
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0')
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5002)
